8b08a53234bc6e1579161388ef3002faa1fa353d
Federico Heredia <federicomartinheredia2006@gmail.com>
2022-06-15 01:09:45 -0300

    fixtureando

---
+++ main.js
@@ -0,0 +1,53 @@
+function handleSubmit(event) {
+  event.preventDefault();
+  let local = event.target.children[0].textContent;
+ let visitante = event.target.children[3].textContent;
+ let golesLocal = event.target.children[1].value;
+ let golesVisitante = event.target.children[2].value;
+
+ let ganoLocal = false;
+ let empate = false;
+ if (golesLocal > golesVisitante) ganoLocal = true;
+ else if (golesLocal == golesVisitante) empate = true;
+ if (golesLocal < golesVisitante) {let ganoVisitante = true;
+}
+
+ // console.log(`${local} ${golesLocal} | ${golesVisitante} ${visitante}`);
+ let tds = document.querySelectorAll('td');
+ for (let td of tds) {
+   if (td.textContent == local || td.textContent == visitante) {
+     // si empatan
+     if (empate) {
+       td.parentElement.children[5].textContent = parseInt(td.parentElement.children[5].textContent) + 1;
+     }
+     // PJ + 1
+     td.parentElement.children[3].textContent = parseInt(td.parentElement.children[3].textContent) + 1;
+   }
+   if (td.textContent == local) {
+     // GF del local
+     td.parentElement.children[7].textContent = parseInt(td.parentElement.children[7].textContent) + parseInt(golesLocal);
+     // GC del local
+     td.parentElement.children[8].textContent = parseInt(td.parentElement.children[8].textContent) + parseInt(golesVisitante);
+   }
+   if (td.textContent == visitante) {
+     // GF del visitante
+     td.parentElement.children[7].textContent = parseInt(td.parentElement.children[7].textContent) + parseInt(golesVisitante);
+     // GC del visitante
+     td.parentElement.children[8].textContent = parseInt(td.parentElement.children[8].textContent) + parseInt(golesLocal);
+     if (ganoVisitante) {
+       parseInt(d.parentElement.children[4].textContent) + 1;
+     }
+   }
+   if (td.textContent == local) {
+     if (ganoLocal) {
+       td.parentElement.children[4].textContent = parseInt(td.parentElement.children[4].textContent) + 1 ;
+     }
+   if (td.textContent == visitante) {
+     if (ganoVisitante) {
+       parseInt(d.parentElement.children[4].textContent) + 1;
+     }
+   }
+   }
+ }
+
+}
